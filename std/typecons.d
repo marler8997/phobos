0071d941a1afc9f14b3ad2ab4b113da167e8dc9a
Jonathan Marler <jonathan.j.marler@hp.com>
2018-03-12 11:34:55 -0600

    Make Nullable a template so that it can be modified to "select" different implementations.

--- std/typecons.d
+++ std/typecons.d
@@ -2333,7 +2333,24 @@ non-null. Calling $(D nullify) can nullify it again.
 
 Practically $(D Nullable!T) stores a $(D T) and a $(D bool).
  */
-struct Nullable(T)
+template Nullable(T)
+{
+    alias Nullable = NullableWithExtraField!T;
+}
+
+
+/**
+Defines a value paired with a distinctive "null" state that denotes
+the absence of a value. If default constructed, a $(D Nullable!T)
+object starts in the null state. Assigning it renders it non-null.
+Calling $(D nullify) can nullify it again.
+
+Practically $(D Nullable!T) stores a $(D T) and a $(D bool). Note that being
+nullified is not the same as assigning the value to a `null` value.
+For example, if `T` is a class, assigning the value to `null` does not nullify
+this struct.
+ */
+struct NullableWithExtraField(T)
 {
     // simple case: type is freely constructable
     static if (__traits(compiles, { T _value; }))
